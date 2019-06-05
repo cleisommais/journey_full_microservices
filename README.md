65a625c3355a72c516366a890fdc075ef1fbf0a6
cleisommais <cleisommais@gmail.com>
2019-06-05 06:46:20 +0100

    Added the new component product-edit to microservice-front-end-product.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# journey_full_microservices
+# The journey to building a full microservice app
+
+### Author: Cleison Ferreira de Melo @cleisommais
+
+ I will share my journey creating a POC to build a full microservice app. Full microservice means that I plan to build components in the front-end and back-end side separately, **to help CI/CD process** inside of the agile teams.
+
+ ### Links articles published
+
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-begin-ferreira-de-melo/
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-dev-env-ferreira-de-melo/
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-model-routes-ferreira-de-melo/
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-docker-compose-cleison/
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-implementing-ferreira-de-melo/
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-front-end-ferreira-de-melo/
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-routes-ferreira-de-melo/
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-products-ferreira-de-melo/
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-product-add-ferreira-de-melo/ 
+* https://www.linkedin.com/pulse/journey-building-full-microservice-app-component-ferreira-de-melo/
+
+### Important to know
+
+The details about the projects are documented inside each project
+
+> Success is a journey, not a destination. The doing is often more important than the outcome. Arthur Ashe
+
+_Good luck_
--- microservice-front-end-product/src/app/product-edit/product-edit.component.ts
+++ microservice-front-end-product/src/app/product-edit/product-edit.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../product-service/product.service';
+import { MatSnackBar } from '@angular/material';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
-  styleUrls: ['./product-edit.component.css']
+  styleUrls: ['./product-edit.component.css'],
 })
 export class ProductEditComponent implements OnInit {
+  productForm: FormGroup;
+  _id: number = null;
+  name: string = '';
+  value: number = null;
+  quantity: number = null;
+  isLoadingResults = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private api: ProductService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
+    this.getProduct(this.route.snapshot.params['id']);
+    this.productForm = this.formBuilder.group({
+      name: [null, Validators.required],
+      value: [null, Validators.required],
+      quantity: [null, Validators.required],
+    });
   }
 
+  onFormSubmit(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.updateProduct(this._id, form).subscribe(
+      res => {
+        this.openSnackBar('Product updated', 'Info');
+        let id = res['_id'];
+        this.isLoadingResults = false;
+        this.router.navigate(['/product-details', id]);
+      },
+      err => {
+        this.openSnackBar(err.error.message.errmsg, 'Error');
+        console.log(err);
+        this.isLoadingResults = false;
+      }
+    );
+  }
+
+  productDetails() {
+    this.router.navigate(['/product-details', this._id]);
+  }
+
+  getProduct(id) {
+    this.api.getProduct(id).subscribe(data => {
+      this._id = data._id;
+      this.productForm.setValue({
+        name: data.name,
+        value: data.value,
+        quantity: data.quantity,
+      });
+    });
+  }
+
+  openSnackBar(message: any, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 5000,
+      verticalPosition: 'top',
+      horizontalPosition: 'end',
+    });
+  }
 }
