9146e52349beae4c37463bcd0ca7aa721df68216
Cleison Ferreira de Melo <cleisommais@gmail.com>
2019-06-19 21:34:04 +0100

    Converted the front-end-report to use single-spa

--- microservice-front-end-report/src/app/app-routing.module.ts
+++ microservice-front-end-report/src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportsComponent } from './reports/reports.component';
+import { APP_BASE_HREF } from '@angular/common';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 const routes: Routes = [
   {
-    path: 'reports',
+    path: '',
     component: ReportsComponent,
     data: { title: 'Dashboard/Reports' },
   },
-  { path: '', redirectTo: '/reports', pathMatch: 'full' },
+  { path: '**', component: EmptyRouteComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
 })
 export class AppRoutingModule {}
