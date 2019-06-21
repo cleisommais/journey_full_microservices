b8dee5489bcb47deb6377b7c5e748cad1852fd2d
Cleison Ferreira de Melo <cleisommais@gmail.com>
2019-06-21 06:29:45 +0100

    Added the new fron-end projects in front-end wrapper

--- front-end-wrapper/index.js
+++ front-end-wrapper/index.js
@@ -10,5 +10,23 @@ System.import('single-spa').then(function(singleSpa) {
 			location.pathname.startsWith('/consumer-details/') ||
 			location.pathname.startsWith('/consumer-edit/')
 	);
+	singleSpa.registerApplication(
+		'app-product',
+		() => System.import('app-product'),
+		location =>
+			location.pathname.startsWith('/products') ||
+			location.pathname.startsWith('/product-add') ||
+			location.pathname.startsWith('/product-details/') ||
+			location.pathname.startsWith('/product-edit/')
+	);
+	singleSpa.registerApplication(
+		'app-order',
+		() => System.import('app-order'),
+		location =>
+			location.pathname.startsWith('/orders') ||
+			location.pathname.startsWith('/order-add') ||
+			location.pathname.startsWith('/order-details/') ||
+			location.pathname.startsWith('/order-edit/')
+	);
 	singleSpa.start();
 });
